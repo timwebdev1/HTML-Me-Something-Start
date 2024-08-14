fd56be535374a177d97d2dfb0aec89b7a5c1fe43
Timothy McDermott <tlmcdermott79@gmail.com>
2024-08-13 22:41:23 -0500

    introduced full page tabs

---
+++ script.js
@@ -0,0 +1,26 @@
+function openPage(pageName, elmnt, color, imageUrl) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  // Show the specific tab content and set its background image
+  // document.getElementById(pageName).style.display = "block";
+  const tab = document.getElementById(pageName);
+  tab.style.display = "block";
+  tab.style.backgroundImage = `url(${imageUrl})`;
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();

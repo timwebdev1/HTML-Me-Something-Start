4160051a9e274a13d9752ee43ff8bc9c4b549a2a
Timothy McDermott <tlmcdermott79@gmail.com>
2024-08-14 09:37:38 -0500

    completed html and css

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function openPage(pageName, elmnt, color, imageUrl) {
   // Hide all elements with class="tabcontent" by default
+ 
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
